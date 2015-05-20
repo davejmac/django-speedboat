00d43231b97c6379f11da04e628245d4f5a29957
davejmac <david.mcnamara@outlook.com>
2015-05-20 15:47:56 -0400

    mixing it up a bit for running the test runner directly

--- testrunner.py
+++ testrunner.py
@@ -13,8 +13,14 @@ django.setup()
 def run_tests():
     runner_class = get_runner(settings)
     test_runner = runner_class(verbosity=1, interactive=True)
-    failures = test_runner.run_tests(['testapp'])
+    failures = test_runner.run_tests(['tests'])
     sys.exit(failures)
 
-if __name__ == '__main__':
-    run_tests()
+
+if __name__ == "__main__":
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.test_settings'
+    django.setup()
+    TestRunner = get_runner(settings)
+    test_runner = TestRunner()
+    failures = test_runner.run_tests(['tests'])
+    sys.exit(bool(failures))
